96b3aa49948d0139db98ae5ecb9d269fe2a2243d
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-11-01 00:57:58 -0400

    Refactor and new MiniMax.
    Refactored to use new statistics database.
    
    Able to calculate score for many players.
    
    Calculating score at leaves instead of rolling.

--- base.py
+++ base.py
@@ -1,7 +1,8 @@
 from ..client.algorithm import Algorithm, Heuristic
-from ..common import PlayPacket, Move, Board, Tile, make_line
+from ..common import PlayPacket, MovePacket, MoveStatistics, Tile, make_line
 from typing import List, Tuple, Set, Union
 import random
+import itertools
 
 # TODO: Implement these classes.
 
@@ -9,9 +10,9 @@ class MiniMax(Algorithm):
     def __get_tile_positions(self, tiles: Set[Tuple[int, int, Tile]]) -> Set[Tuple[int, int]]:
         return set([(x, y) for x, y, _ in tiles])
 
-    def __get_moves(self, moves: List[Tuple[int, int, Tile]], blocks: Set[Tuple[int, int]]) -> Set[Move]:
+    def __get_moves(self, moves: List[Tuple[int, int, Tile]]) -> Set[Tuple[int, int]]:
         tile_positions = self.__get_tile_positions(moves)
-        invalid = blocks | tile_positions
+        invalid = tile_positions | self.blocks
         moves = set()
 
         for (x, y) in tile_positions:
@@ -25,141 +26,129 @@ class MiniMax(Algorithm):
 
         return moves
 
-    def __get_tile(self, is_self: bool) -> Tile:
-        if is_self:
-            return self.tile
-        else:
-            return Tile.WHITE if self.tile == Tile.BLACK else Tile.BLACK
+    def __next_tile(self, tile: Tile) -> Tile:
+        return self.order[(self.order.index(tile) + 1) % len(self.order)]
 
-    def __dp(self, depth: int, moves: List[Tuple[int, int, Tile]], blocks: Set[Tuple[int, int]], is_self: bool=True) -> Union[Move, int]:
-        if depth == 0:
-            return 0
-
-        tile = self.__get_tile(is_self)
-        next_moves = list(self.__get_moves(
-            moves=moves,
-            blocks=blocks
-        ))
-
-        scores = []
-        for (x, y) in next_moves:
-            next_score = self.get_score(
-                data={"moves": moves, "blocks": blocks, "move": (x, y, tile), "depth": depth}
-            )
-
-            if depth == self.max_depth and abs(next_score) == 10 ** 10:
-                return (x, y)
-            elif next_score == 10 ** 10:
-                return next_score + depth
-            elif next_score == -(10 ** 10):
-                return next_score - depth
-
-            nt = moves.copy()
-            nt.append((x, y, tile))
-
-            scores.append(next_score + self.__dp(
-                depth=depth - 1, 
-                moves=nt,
-                blocks=blocks,
-                is_self=not is_self
-            ))
-
-        if depth == self.max_depth:
-            if len(scores) == 0:
-                invalid = blocks | self.__get_tile_positions(moves)
-                next_moves = set()
-
-                for y in range(self.board_size):
-                    for x in range(self.board_size):
-                        next_moves.add((x, y))
-
-                next_moves = next_moves - invalid
-
-                return random.choice(list(next_moves))
-
-            max_score = max(scores)
-            max_index = scores.index(max_score)
-
-            return next_moves[max_index]
-        elif len(scores) == 0:
-            return 0
-        elif is_self:
-            return max(scores)
-        else:
-            return min(scores)
-
-    def next_move(self, packet: PlayPacket) -> Move:
-        move = self.__dp(
-            depth=self.max_depth, 
-            moves=packet.moves,
-            blocks=set(packet.blocks),
-            is_self=True
-        )
+    def __random_move(self, moves: List[Tuple[int, int, Tile]]) -> Tuple[int, int]:
+        invalid = self.__get_tile_positions(tiles=moves) | self.blocks
+        empty = set([(x, y) for x, y in itertools.product(range(self.board_size), range(self.board_size))]) - invalid
+        move = random.choice(list(empty))
 
         return move
 
+    def __print_board(self, moves: List[Tuple[int, int, Tile]], depth: int):
+        board = [["-" for _ in range(self.board_size)] for __ in range(self.board_size)]
+
+        for x, y in self.blocks:
+            board[y][x] = "X"
+
+        for x, y, tile in moves:
+            board[y][x] = tile.value
+
+        for d, (x, y, tile) in enumerate(reversed(moves[-depth:])):
+            board[y][x] = f"({tile.value}-{d})"
+
+        for row in board:
+            print('\t'.join(str(tile) for tile in row))
+
+    def __get_tile_score(self, score: List[float], index: int) -> float:
+        score_copy = score.copy()
+        self_score = score_copy.pop(index)
+        max_other_score = max(score_copy)
+
+        return self_score - max_other_score
+
+    def __search(self, depth: int, moves: List[Tuple[int, int, Tile]], tile: Tile) -> Tuple[Tuple[int, int], List[float]]:
+        if depth == 0:
+            return (None, self.get_score({
+                "moves": moves,
+                "depth": self.max_depth - depth,
+                "order": self.order,
+                "blocks": self.blocks
+            }))
+
+        max_nexts = []
+        for x, y in self.__get_moves(moves=moves):
+            move = (x, y, tile)
+            max_move, max_score = self.__search(
+                depth=depth-1,
+                moves=moves + [move],
+                tile=self.__next_tile(tile=tile)
+            )
+            max_nexts.append((move, max_score))
+
+        tile_index = self.order.index(tile)
+
+        if len(max_nexts) == 0:
+            return self.__random_move(moves=moves), [0] * len(self.order)
+
+        max_move, max_score = max(max_nexts, key=lambda move_score: self.__get_tile_score(score=move_score[1], index=tile_index))
+
+        # print("Depth:", self.max_depth - depth)
+        # print("Score:", max_score)
+        # self.__print_board(moves=moves + [max_move], depth=self.max_depth - depth + 1)
+
+        return (max_move, max_score)
+
+    def next_move(self, packet: PlayPacket) -> MovePacket:
+        self.blocks = set(packet.blocks)
+        self.order = packet.order
+
+        move, score = self.__search(
+            depth=self.max_depth, 
+            moves=packet.moves,
+            tile=self.tile
+        )
+
+        print("Play:", move, "Score:", score)
+
+        return MovePacket(
+            move=(move[0], move[1])
+        )
+
 class AlphaBeta(Algorithm):
-    def next_move(self, packet: PlayPacket) -> Move:
-        return (0, 0)
+    def next_move(self, packet: PlayPacket) -> MovePacket:
+        return MovePacket(
+            move=(0, 0)
+        )
 
 class Heuristic1(Heuristic):
-    def __get_tile(self, tile: Union[Tile, int]) -> Tile:
-        return Tile(tile) if isinstance(tile, int) else tile
-    
-    def __other_tile(self, tile: Tile) -> Tile:
-        return Tile.BLACK if tile == Tile.WHITE else Tile.WHITE
-
-    def get_score(self, data: any) -> float:
-        tiles: List[Tuple[int, int, Tile]] = data["moves"]
+    def get_score(self, data: any) -> List[float]:
+        moves: List[Tuple[int, int, Tile]] = data["moves"]
         blocks: Set[Tuple[int, int]] = data["blocks"]
+        depth: int = data["depth"]
+        order: List[Tile] = data["order"]
 
-        tile_positions = {
-            Tile.WHITE: set(),
-            Tile.BLACK: set()
-        }
+        invalid: Set[Tuple[int, int]] = set([(x, y) for x, y, _ in moves]) | blocks
 
-        for x, y, tile in tiles:
-            tile_positions[self.__get_tile(tile)].add((x, y))
+        score = [0] * len(order)
+        for i, (x, y, tile) in enumerate(moves[-depth:]):
+            tile_index = order.index(tile)
+            local_depth = self.max_depth - i + 1
+            local_score = 0
 
-        player_scores = {
-            Tile.WHITE: 0,
-            Tile.BLACK: 0
-        }
+            for dx, dy in [(1, 0), (0, 1), (1, 1), (1, -1)]:
+                for offset in range(self.line_up_size + 1):
+                    line = make_line(
+                        point=(x - dx * offset, y - dy * offset), 
+                        direction=(dx, dy), 
+                        length=self.line_up_size,
+                        board_size=self.board_size
+                    )
 
-        x, y, tile = data["move"]
-        for direction in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-            line_up_size = self.line_up_size
+                    if not line: continue
 
-            line = make_line(
-                point=(x, y), 
-                direction=direction, 
-                length=line_up_size,
-                board_size=self.board_size
-            )
-
-            if not line: continue
-
-            line_count = 0
-            empty_count = 0
-
-            for lx, ly in line:
-                if (lx, ly) in tile_positions[tile]:
-                    line_count += 1
-                if (lx, ly) in tile_positions[self.__other_tile(tile)] or (lx, ly) in blocks:
                     line_count = 0
-                    break
-                else:
-                    empty_count += 1
+                    for lx, ly in line:
+                        if (lx, ly, tile) in moves:
+                            line_count += 1
+                        elif (lx, ly) in invalid:
+                            line_count = 0
+                            break
 
-            if line_count + min(1, empty_count) >= line_up_size:
-                player_scores[tile] = 10 ** 10
-                break
-            else:
-                player_scores[tile] += (line_count + empty_count) ** (11 - line_up_size)
+                    local_score = max(local_score, min(self.line_up_size, line_count))
 
-        my_tile = self.tile
-        other_tile = self.__other_tile(my_tile)
-        score = player_scores[my_tile] - player_scores[other_tile]
+            score[tile_index] += local_score ** local_depth
 
         return score
 
